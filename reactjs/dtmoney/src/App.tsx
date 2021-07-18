9ae198f96b468c8b200d80e2a63dec8236cd96c4
Jonathan Santos <jonathanlpsantos@outlook.com>
2021-07-18 18:14:46 -0300

    Importando fonts do Google e criando os estilos globais

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -1,15 +1,12 @@
-import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
 
-const Title = styled.h1`
-  font-size: 100px;
-  color: #8257e6;
-`
 
 export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Title className="title">Hello, world</Title>
+        <h1>Hello world</h1>
+        <GlobalStyle />
       </header>
     </div>
   );
