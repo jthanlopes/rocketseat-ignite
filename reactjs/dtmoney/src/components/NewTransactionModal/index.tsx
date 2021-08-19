6d9c4fc9ee59cc1f15a9e26f8dd6fd4b7f9eb505
Jonathan Santos <jonathanlpsantos@outlook.com>
2021-08-19 12:03:40 -0300

    Criação e formatação das tags internas do modal

--- reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,8 @@
 import Modal from 'react-modal';
+import { Container, TransactionTypeContainer } from './styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 interface NewTransactionModalProps {
 		isOpen: boolean;
@@ -12,8 +16,51 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 		<Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar transação</h2>
+      <button
+        type="button"
+        onClick={onRequestClose} className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar modal"/>
+      </button>
+
+    	<Container>
+      	<h2>Cadastrar transação</h2>
+
+      	<input
+      		type="text"
+      		placeholder="Título"
+      	/>
+      	<input
+      		type="number"
+      		placeholder="Valor"
+      	/>
+
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button
+            type="button"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
+      	<input
+      		placeholder="Categoria"
+      	/>
+
+      	<button type="submit">
+      		Cadastrar
+      	</button>
+      </Container>
     </Modal>
-	)
+	);
 }
