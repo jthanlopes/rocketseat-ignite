76f65881581d9890918c0fcdd762afae7f1725ff
Jonathan Santos <jonathanlpsantos@outlook.com>
2021-11-09 12:07:04 -0300

    Title dinâmico do app

--- reactjs/ignews/src/pages/_document.tsx
+++ reactjs/ignews/src/pages/_document.tsx
@@ -6,10 +6,8 @@ export default class MyDocument extends Document {
 			<Html>
 				<Head>
 					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
 					<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-
-					<title>ig.news</title>
 				</Head>
 				<body>
 					<Main />
