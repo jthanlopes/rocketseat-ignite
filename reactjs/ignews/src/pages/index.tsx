19ebf806061921b8087a911cbbf753e1d11e67cd
Jonathan Santos <jonathanlpsantos@outlook.com>
2021-11-09 12:24:09 -0300

    Estilos globais do app

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return [
       <Head>
           <title>Início | ig.news</title>
       </Head>,
-      <h1 className={styles.title}>
-        Hello, <span>world!</span>
+      <h1>
+        Hello, world!
       </h1>
   ]
 }
