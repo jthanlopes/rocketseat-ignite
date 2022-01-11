efcba95d9d6488b939313857a39d29f6201b22b9
Jonathan Santos <jonathanlpsantos@outlook.com>
2022-01-11 11:21:03 -0300

    Validação do login com o try/catch

--- reactjs/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/ignews/src/pages/api/auth/[...nextauth].ts
@@ -21,30 +21,34 @@ export default NextAuth({
     async signIn({user, account, profile}) {
       const { email } = user;
 
-      await fauna.query(
-        q.If(
-          q.Not(
-            q.Exists(
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+                { data: { email } }
+            ),
+            q.Get(
               q.Match(
                 q.Index('user_by_email'),
                 q.Casefold(user.email)
               )
             )
-          ),
-          q.Create(
-            q.Collection('users'),
-              { data: { email } }
-          ),
-          q.Get(
-            q.Match(
-              q.Index('user_by_email'),
-              q.Casefold(user.email)
-            )
           )
         )
-      )
 
-      return true;
+        return true;
+      } catch {
+        return false;
+      }
     },
   }
 })
