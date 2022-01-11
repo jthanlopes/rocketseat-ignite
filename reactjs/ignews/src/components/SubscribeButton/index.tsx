5cdec2508c24c84d445f4102f5fedaefdd66e710
Jonathan Santos <jonathanlpsantos@outlook.com>
2022-01-11 11:59:29 -0300

    Gerando sessão de checkout com o stripe

--- reactjs/ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import { useSession, signIn } from 'next-auth/react';
+
 import styles from './styles.module.scss';
 
 interface SubsbribeButtonProps {
@@ -5,10 +7,22 @@ interface SubsbribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubsbribeButtonProps) {
+	const {data: session} = useSession();
+
+	function handleSubscribe() {
+		if(!session) {
+			signIn('github');
+			return;
+		}
+
+		
+	}
+
 	return (
 		<button
 			type="button"
 			className={styles.subscribeButton}
+			onClick={handleSubscribe}
 		>
 			Subsbribe now
 		</button>
